d22279244e52fb00f62aa62a058f50de45cc917e
wakenedo <a_nuner@outlook.com>
2021-11-02 15:31:57 -0300

    Styling - Part 1

---
+++ src/components/Footer/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import {
+    FooterContainer,
+    FooterWrap,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    SocialIcons,
+    SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>Pizza</SocialLogo>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Twitter" rel="noopener noreferrer">
+                                <FaTwitter />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer
---
+++ src/components/Products/data.js
@@ -0,0 +1,62 @@
+import product1 from '../../images/product1.jpg';
+import product2 from '../../images/product2.jpg';
+import product3 from '../../images/product3.jpg';
+import sweet1 from '../../images/Sweet1.jpg';
+import sweet2 from '../../images/Sweet2.jpg';
+import sweet3 from '../../images/Sweet3.jpg';
+
+
+
+export const productData = [
+    {
+        img:product1,
+        alt:'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+        price: '$19.99',
+        button: 'Add to Cart'
+    },
+    {
+        img:product2,
+        alt:'Pizza',
+        name: 'Hawaiian Paradise',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+        price: '$16.99',
+        button: 'Add to Cart'
+    },
+    {
+        img:product3,
+        alt:'Pizza',
+        name: 'Veggie Overload',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+        price: '$14.99',
+        button: 'Add to Cart'
+    }
+]
+
+export const productDataTwo = [
+    {
+        img:sweet1,
+        alt:'Donuts',
+        name: 'Doughlicious',
+        desc: 'Belgian chocolate glazed donuts covered in icing with sparkles on top',
+        price: '$9.99',
+        button: 'Add to Cart'
+    },
+    {
+        img:sweet2,
+        alt:'Ice Cream',
+        name: 'Caramel Wonder',
+        desc: 'Vanilla Ice Cream covered with caramel and chocolate glaze topped with a coco stick',
+        price: '$12.99',
+        button: 'Add to Cart'
+    },
+    {
+        img:sweet3,
+        alt:'Brownie',
+        name: 'Brownie Bunch',
+        desc: 'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+        price: '$11.99',
+        button: 'Add to Cart'
+    }
+]
